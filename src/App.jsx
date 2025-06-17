8d842762fb71a166fd2daa8ed2aeac574aa61c5f
Sadwalps <sadwalps2255@gmail.com>
2025-06-17 19:45:21 +0530

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,25 @@ function App() {
   const [area, setArea] = useState("")
   const [areavalue, Setareavalue] = useState(0)
 
- const validate = (e)=>{
-  const {name,value} = e.target
-  console.log(name);
-  console.log(value);
-
-  if(!!value.match('^[0-9]*$')){
+  const validate = (e) => {
+    const { name, value } = e.target
+    console.log(name);
+    console.log(value);
+   if(!!value.match('^[0-9]*$')){
     if(name==radius){
       setRadius(value)
     }else{
-       setArea(value)
+      setArea(value)
     }
+   }
   }
- }
+
+  const handleSubmit = () =>{
+   Setareavalue(3.14*(radius*radius*radius) )
+  }
+  console.log(areavalue);
+  
+
 
   return (
     <>
@@ -32,20 +38,20 @@ function App() {
             <div className='p-3 text-center rounded bg-primary'>
               <h2 className='pt-2' style={{ color: "white", fontWeight: "900" }}>Let's calculate area of circle</h2>
               <div className='d-flex justify-content-center'>
-                <input type="text" onChange={(e)=>validate(e)}  name='radius' className='form-control w-75 mt-3' placeholder='Enter the radius of the circle' />
+                <input type="text" onChange={(e) => validate(e)} name='radius' className='form-control w-75 mt-3' placeholder='Enter the radius of the circle' />
               </div>
 
               {/* result section*/}
               <div className=" d-flex justify-content-center align-items-center">
                 <div id='resultcircle' className='mt-4 mb-3 d-flex flex-column justify-content-center align-items-center' >
                   <h3>Result</h3>
-                  <h4 onChange={(e)=>validate(e)} name='area'></h4>
+                  <h4 onChange={(e) => validate(e)} name="area" >{areavalue}</h4>
                 </div>
               </div>
 
               {/* buttons */}
               <div className='d-flex justify-content-around'>
-                <button className='btn btn-success rounded-circle ' style={{ height: "50px", width: "50px" }}>
+                <button className='btn btn-success rounded-circle ' style={{ height: "50px", width: "50px" }} onClick={handleSubmit}>
                   <FontAwesomeIcon icon={faArrowRight} />
                 </button>
                 <button className='btn btn-danger rounded-circle ' style={{ height: "50px", width: "50px" }}>
